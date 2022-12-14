a90de450d1ffc0114bf2acb1837193ca18082def
celuu <christine.e.luu@gmail.com>
2022-12-14 12:19:00 -0800

    Coffee Beans Started

--- practice-for-week-14-react-context-review-with-tests-long-practice/src/App.js
+++ practice-for-week-14-react-context-review-with-tests-long-practice/src/App.js
@@ -1,11 +1,17 @@
 import coffeeBeans from "./mockData/coffeeBeans.json";
+import SelectedCoffeeBean from "./components/SelectedCoffeeBean";
+import { CoffeeContext, CoffeeProvider } from "./context/CoffeeContext";
 
 function App() {
-  return (
-    <>
-      <h1>Welcome to Coffee App</h1>
-    </>
-  );
+  
+    return (
+      <>
+        <h1>Welcome to Coffee App</h1>
+
+          <SelectedCoffeeBean />
+
+      </>
+    );
 }
 
 export default App;
--- practice-for-week-14-react-context-review-with-tests-long-practice/src/components/SelectedCoffeeBean.js
+++ practice-for-week-14-react-context-review-with-tests-long-practice/src/components/SelectedCoffeeBean.js
@@ -1,5 +1,17 @@
+import {CoffeeContext} from '../context/CoffeeContext'
+import { useState, useContext } from 'react';
+
 const SelectedCoffeeBean = () => {
-  return null;
+  const [coffeeBean, setCoffeeBean] = useState({});
+  const desiredBean = useContext(CoffeeContext);
+  setCoffeeBean(desiredBean);
+  
+  return (
+    <div className='selected-coffee'>
+      <h2>{coffeeBean}</h2>
+    </div>
+  );
+
 }
 
 export default SelectedCoffeeBean;
